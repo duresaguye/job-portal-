9d5872f559c3605393d6249ca03fd08c34a54b82
dura G <dureguye2@gmail.com>
2024-01-05 01:50:44 +0300

    sidebar added

--- asset/script.js
+++ asset/script.js
@@ -49,16 +49,8 @@ function toggleForm(activeForm) {
       signInForm.classList.remove('hidden');
     }
   }
-  document.addEventListener('DOMContentLoaded', function () {
-    var mobileMenu = document.getElementById('mobile-menu');
-    var nav = document.querySelector('nav');
-
-    if (mobileMenu) {
-        mobileMenu.addEventListener('click', function () {
-           
-            nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-        });
-    }
-});
-
+  function Menu(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+  }
 
